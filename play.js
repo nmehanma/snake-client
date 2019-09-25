35147a55170667738f6a207c9a5d1760a6468597
Najeam <nmehanma@gmal.com>
2019-09-25 17:56:30 +0000

    edited

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
 const { connect } = require('./client');
 
 
-
 console.log('connecting..');
+connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+}
+
+const handleUserInput  = (input) => {
+  input.on('data', key => {
+    if (key === '\u0003') {
+      process.exit();
+    };
+})
+};
+
+setupInput();
 connect();
