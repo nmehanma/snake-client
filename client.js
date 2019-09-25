a125a7aff9f0c988636c90220bfed02c526dd827
Najeam <nmehanma@gmal.com>
2019-09-25 17:17:46 +0000

    edited

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.2.204',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on("data", (data) => {
+    console.log(`Server says ${data}`)
+  })
+
+  return conn;
+}
+
+
+
+module.exports = {connect};
+
+
+
