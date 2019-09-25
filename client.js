939538c4dd9a85a5cac9223d498c7730cd01a6ff
Najeam <nmehanma@gmal.com>
2019-09-25 17:33:02 +0000

    edited

--- client.js
+++ client.js
@@ -8,11 +8,20 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on("connect", () => {
+    console.log("succesfuly connected to the game server")
+    conn.write("Name: NM");
+
+  });
+
   conn.on("data", (data) => {
     console.log(`Server says ${data}`)
-  })
 
-  return conn;
+  });
+
+  
+  
+   return conn;
 }
 
 
--- play.js
+++ play.js
@@ -1,18 +1,6 @@
-const net = require('net');
+const { connect } = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
-  return conn;
-}
 
-console.log('you ded cuz you idled');
+console.log('connecting..');
 connect();
